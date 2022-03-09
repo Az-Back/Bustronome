59bfc7cc37f5578af20f14ffbfe99e77718b8b44
Az-Back <back.ranged@gmail.com>
2022-03-09 11:33:42 +0100

    first commit

---
+++ includes/navbar.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="../styles/navbar.css">
+<script src="https://kit.fontawesome.com/10a6ffe905.js" crossorigin="anonymous"></script>
+<nav class="navbar">
+  <div class="LogoNav">
+    <a href="https://www.bustronome.com./fr/"><img class="LogoNavImage" src="../images/logo.png"></a>
+</div>
+  <ul>
+    <li class="TextNav"><a class="RefNav active" href="">RÉSERVEZ MAINTENANT</a></li>
+    
+    <li class="TextNav"><a class="RefNav" href="">☆ CARTE CADEAU ☆</a></li>
+    
+    <li class="TextNav"><a class="RefNav" href="">LES OFFRES</a></li>
+    
+    <li class="TextNav"><a class="RefNav" href="">LE CONCEPT</a></li>
+    
+    <li class="TextNav"><a class="RefNav" href="">PRIVATISATION</a></li>
+    
+    <li class="TextNav"><a class="RefNav" href="">ACTUALITÉS DU BUS</a></li>
+    
+    <li class="TextNav"><a class="RefNav" href="">LE BLOG</a></li>
+    
+    <li class="TextNav"><a class="RefNav" href="">PHOTOS ET VIDÉOS</a></li>
+    
+    <li class="TextNav"><a class="RefNav" href="">QUESTIONS FRÉQUENTES</a></li>
+    
+    <li class="TextNav"><a class="RefNav" href="">CONTACTEZ LE BUSTRONOME PARIS</a></li>
+  </ul>
+  <div class="place">
+    <div class="Icon"><i class="fa-solid fa-location-dot fa-3x"></i></div>
+    <div class="links-place">
+      <span>ARC DE TRIOMPHE</span>
+    <a class="RefNav2" href="">angle avenue Kleber</a></div>
+  </div>
+</nav>
